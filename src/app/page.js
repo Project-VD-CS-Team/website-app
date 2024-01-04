edb0111dd3f72f0844c7c532b8828aa99691e8c2
Bala Rajakumar <133478378+Balavinayagam1999@users.noreply.github.com>
2024-01-04 14:36:12 -0800

    Updated page.js
    Just added some sections of paragraphs into main page describing project.

--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,21 @@ export default function Home() {
   return (
     <>
       <div className="container">
-        <h1>Welcome!</h1>
+        <h1>About us </h1>
         <main>
-          <section id = "summarySection">
-              <h2 className = "sectionHeader">Summary</h2>
-              <article id = "summaryBody">
-                <h3>Visual Detector is a device that is capable of detecting microbes through the use of computer vision and deep learning</h3>
-              </article>
-          </section>
           <section id = "projectStatement">
               <h2 className = "sectionHeader">Purpose</h2>
-              <article id = "statementHeader">Main Body</article>
+              <article id = "statementHeader">
+                <h3>Visual Detector is a device that is capable of detecting microbes through the use of computer vision and deep learning</h3>
+                <h3> 
+                 Our project is a unique and innovative solution that aims to solve a problem in a particular industry.
+                 It is designed to provide a comprehensive and user-friendly experience to our customers. 
+                 Our team has worked tirelessly to ensure that the project meets the highest standards of quality and functionality. 
+                 We are confident that our project will be a game-changer in the industry and will set a new standard for excellence. 
+                 We are excited to share our project with the world and look forward to seeing the positive impact 
+                 it will have on our customers and the industry as a whole.
+                </h3>
+              </article>
           </section>
         </main>
       </div>
