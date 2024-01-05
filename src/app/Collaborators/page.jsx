32ab9aa3b8f089a69fe249414c1ae873ffa1d5f7
Bala Rajakumar <133478378+Balavinayagam1999@users.noreply.github.com>
2024-01-05 12:42:12 -0800

    Collaboration Page
    Added names of some members to test out flex box, will need to space them properly

--- src/app/Collaborators/page.jsx
+++ src/app/Collaborators/page.jsx
@@ -4,9 +4,18 @@ export default function Collab(){
 
     return(
         <div className="container">
-            <h1>Collaboration Page</h1>
             <main>
-                <h2>Body</h2>
+                <div class="flex-container">
+                    <div>Bala Rajakumar</div>
+                    <div>Paul Colonia</div>
+                    <div>Vitaly Veksler</div>
+                    <div>Michael Hong</div>
+                    <div>Jin Tang</div>
+                    <div>Abdul </div>
+                    <div>Su tong</div>
+                    <div>test</div>
+                    <div>a</div>
+                </div>
             </main>
         </div>
     )
