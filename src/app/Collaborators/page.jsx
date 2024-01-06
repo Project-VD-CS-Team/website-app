898aecba705932a4f1d93ad19f5b83736b8d815e
Bala Rajakumar <133478378+Balavinayagam1999@users.noreply.github.com>
2024-01-05 16:16:21 -0800

    Collaboration page
    Just added bunch of dialogue

--- src/app/Collaborators/page.jsx
+++ src/app/Collaborators/page.jsx
@@ -6,16 +6,36 @@ export default function Collab(){
         <div className="container">
             <main>
                 <div class="flex-container">
-                    <div>Bala Rajakumar</div>
-                    <div>Paul Colonia</div>
-                    <div>Vitaly Vekslyer</div>
-                    <div>Michael Hong</div>
-                    <div>Jing Tan</div>
-                    <div>Abdurisaq heban</div>
-                    <div>So Yin Tung</div>
-                    <div>Constantine Georgas</div>
-                    <div>Vincent Paulus</div>
-                    <div>Eric Fung</div>
+                    <div>Bala Rajakumar
+                        <h3>Project Lead</h3> 
+                    </div>
+                    <div>Paul Colonia
+                        <h3>Software Team Lead</h3> 
+                    </div>
+                    <div>Vitaly Vekslyer
+                        <h3>Engineering Team Lead</h3> 
+                    </div>
+                    <div>Michael Hong
+                        <h3>Marketing/Bussiness</h3> 
+                    </div>
+                    <div>Jing Tan
+                        <h3>Mechanical Engineer</h3> 
+                    </div>
+                    <div>Abdurisaq heban
+                        <h3>Software Developer</h3> 
+                    </div>
+                    <div>So Yin Tung
+                        <h3>Software Developer</h3> 
+                    </div>
+                    <div>Constantine Georgas
+                        <h3>Mechanical Engineer</h3> 
+                    </div>
+                    <div>Vincent Paulus
+                        <h3>Electrical Engineer</h3> 
+                    </div>
+                    <div>Eric Fung
+                        <h3>Integrated Engineer</h3> 
+                    </div>
 
                 </div>
             </main>
